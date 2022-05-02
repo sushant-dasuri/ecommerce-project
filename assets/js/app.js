234d5a34bd56794c481fac2027d1b8c0a721f2e6
sushant-dasuri <sushant.dasuri@more.global>
2022-05-02 22:18:39 +0530

    Added preloader animation timeout in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,8 @@
-console.log("Working")
+const _body = $('body');
+const _preloader = $("#preloader");
+
+$(window).on('load', function() {
+    _preloader.delay(2000).fadeOut('slow', function(){
+		_body.css({'overflow-y':'unset', 'background':'none'});
+	});
+})
